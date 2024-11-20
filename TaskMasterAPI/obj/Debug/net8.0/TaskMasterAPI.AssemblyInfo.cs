6397eb6b8b917a47df15532b85a75a9d06ca9a64
Leonid19992812 <lbelousov@tennisi.it>
2024-11-20 12:59:31 +0500

    Обновил README.md

--- TaskMasterAPI/obj/Debug/net8.0/TaskMasterAPI.AssemblyInfo.cs
+++ TaskMasterAPI/obj/Debug/net8.0/TaskMasterAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskMasterAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4abf5b15fc5e86cb3494911b30d0935ad3e0e953")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a4c6a757001e65b1e774c2364c3ab51c43265ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskMasterAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskMasterAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
